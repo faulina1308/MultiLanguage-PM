27f179f77a45a1b90404ffc0d4ffa7ebb5c19c2f
faulinaindri <113485041+faulina1308@users.noreply.github.com>
2023-03-23 10:18:07 +0700

    Initial commit

---
+++ app/src/main/java/com/faulinaindri202102297/multilanguage/MainActivity.java
@@ -0,0 +1,14 @@
+package com.faulinaindri202102297.multilanguage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
